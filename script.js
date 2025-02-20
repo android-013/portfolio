8c37aa505756d7bfbb477cb9cf81f5edcc7514d7
android-013 <azrifat013@gmail.com>
2025-02-20 22:10:20 +0600

    x

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener('resize', () => {
 });
 
 // Matrix code logic
-const characters = 'AZIZRIFAT(){}[]/|azizrifat@013';
+const characters = 'AZIZRIFAT@013(){}[]/|azizrifat@013';
 const fontSize = 10;
 const columns = canvas.width / fontSize;
 const drops = Array(Math.floor(columns)).fill(1);
