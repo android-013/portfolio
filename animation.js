8cba4c9bb3d83cd4b993236729d610ca6e79b075
android-013 <azrifat013@gmail.com>
2025-07-11 00:01:24 +0600

    animation js

---
+++ animation.js
@@ -0,0 +1,54 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            if (entry.boundingClientRect.top > entry.rootBounds.top) {
+                entry.target.classList.remove('show');
+            }
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observerBlur = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showBlur');
+        } else {
+            if (entry.boundingClientRect.top > entry.rootBounds.top) {
+                entry.target.classList.remove('showBlur');
+            }
+        }
+    });
+});
+
+const hiddenElementsBlur = document.querySelectorAll('.hiddenBlur');
+hiddenElementsBlur.forEach((el) => observerBlur.observe(el));
+
+
+
+
+const observerSlider = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.width = entry.target.id + '%';
+            entry.target.style.transition =  "all 1s";
+        } else {
+            entry.target.style.width = 0;
+            entry.target.style.transition =  "all 0s";
+        }
+    });
+});
+
+
+function updateProjects() {
+    const h = document.querySelectorAll('.hidden');
+    console.log(h);
+    h.forEach((el) => observer.observe(el));
+}
+
+
+
